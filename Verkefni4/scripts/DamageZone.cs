76a4f2931bd13cc551ca1f19cf8b10479b7d19c1
hedinn1 <97167769+hedinn1@users.noreply.github.com>
2023-05-04 11:51:27 +0000

    Update DamageZone.cs

--- Verkefni4/scripts/DamageZone.cs
+++ Verkefni4/scripts/DamageZone.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    // Þetta fall er kallað þegar hlutur rekst á annan hlut í leiknum sem hefur Collider2D komponent.
     void OnTriggerStay2D(Collider2D other)
     {
+        // Hér er skilgreint controller sem er RubyController hluturinn á Collider2D hlutnum sem rekst á þennan DamageZone hlut.
         RubyController controller = other.GetComponent<RubyController >();
-
+        // Ef RubyController hlutur er á Collider2D hlutnum sem rekst á þennan DamageZone hlut þá er heilsa RubyController hlutarins minnkað um 1.
         if (controller != null)
         {
             controller.ChangeHealth(-1);
